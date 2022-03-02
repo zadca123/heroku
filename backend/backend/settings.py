35376bf0a28bf77c15efeb7c895f5261765c23c5
JRK155460 <100349527+JRK155460@users.noreply.github.com>
2022-03-02 13:19:59 +0100

    Zmiana bazy danych z SQLite na MySQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'procode',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
