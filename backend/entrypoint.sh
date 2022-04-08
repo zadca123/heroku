597e644658772b3ef46fcb1cc08ccc5b8757086e
zadca123 <zadca123@debian-BULLSEYE-live-builder-AMD64>
2022-04-08 15:14:07 +0200

    there is lastly progress

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/bash
-cd /code/backend
+#!/bin/sh
 
-python manage.py migrate --noinput --verbosity 0
-python manage.py collectstatic --noinput --verbosity 0
-
-if [ $USE_VSCODE_DEBUGGER = 1 ]; then
-  python manage.py runserver 0.0.0.0:8000 --noreload --nothreading --verbosity 0
-else
-  python manage.py runserver 0.0.0.0:8000 --verbosity 0
-fi
-
-exec "$@"
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py makemigrations api
+# python manage.py migrate api
+python manage.py runserver 0.0.0.0:8000
